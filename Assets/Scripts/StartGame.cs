7b6d0647db496c6fbb84638389a93dafd4fb8f08
Navjot Kehal <navjotkehal5@gmail.com>
2025-02-03 11:13:04 -0800

    fixed start button so that it makes all the assets visible after pressing the start button

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -6,7 +6,20 @@ public class StartGame : MonoBehaviour
 {
     public GameObject obstacleSpawner;
     public GameObject obstacleSpawner2;
+    public GameObject player;
+    public GameObject playerScore;
+    public GameObject enemy;
+    public GameObject enemyScore;
     public GameObject startButton;
+
+    void Start() {
+        obstacleSpawner.SetActive(false);
+        obstacleSpawner2.SetActive(false);
+        player.SetActive(false);
+        enemy.SetActive(false);
+        playerScore.SetActive(false);
+        enemyScore.SetActive(false);
+    }
 
     public void OnStartButtonClick()
     {
@@ -18,6 +31,12 @@ public class StartGame : MonoBehaviour
         {
             obstacleSpawner2.SetActive(true);
         }
+        player.SetActive(player != null);
+        enemy.SetActive(enemy != null);
+
+        playerScore.SetActive(playerScore != null);
+        enemyScore.SetActive(enemyScore != null);
+        
         if (startButton != null)
         {
             startButton.SetActive(false);

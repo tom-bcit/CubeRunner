9d4c528e9928d351861c1da40531f4d7ad80dc8d
tomhebcit <the18@my.bcit.ca>
2025-02-06 13:22:15 -0800

    added player selection menu

---
+++ Assets/Scripts/PlayerSelection.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelection : MonoBehaviour
+{
+    public GameObject CubeA;
+    public GameObject CubeB;
+    public GameObject HostButton;
+    public GameObject Player1Button;
+    public GameObject StartMenu;
+    public GameObject SelectionMenu;
+
+    public void OnHostButtonClick()
+    {
+        CubeA.GetComponent<MoveCubes>().localCube = CubeA;
+        CubeA.GetComponent<MoveCubes>().remoteCube = CubeB;
+        CubeB.GetComponent<MoveCubes>().localCube = CubeA;
+        CubeB.GetComponent<MoveCubes>().remoteCube = CubeB;
+        SelectionMenu.SetActive(false);
+        StartMenu.SetActive(true);
+    }
+
+    public void OnPlayer1ButtonClick()
+    {
+        CubeA.GetComponent<MoveCubes>().localCube = CubeB;
+        CubeA.GetComponent<MoveCubes>().remoteCube = CubeA;
+        CubeB.GetComponent<MoveCubes>().localCube = CubeB;
+        CubeB.GetComponent<MoveCubes>().remoteCube = CubeA;
+        SelectionMenu.SetActive(false);
+        StartMenu.SetActive(true);
+    }
+
+}
